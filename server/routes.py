a7a556db6f251cf458ded6fb238668260e580988
Andresse Njeungoue <andresse.njeungoue@devoteamgcloud.com>
2018-03-26 15:40:21 +0200

    push all

--- server/routes.py
+++ server/routes.py
@@ -5,11 +5,16 @@ from flask import send_file, send_from_directory
 def add_routes(app):
     app.add_url_rule('/', view_func=index)
     app.add_url_rule('/<path:path>', view_func=send_static_file)
+    app.register_error_handler(404, error)
 
 
 def index():
     return send_file('../dist/index.html', cache_timeout=-1)
-
+    
 
 def send_static_file(path):
+    print (path)
     return send_from_directory('../dist/', path)
+
+def error(e):
+    return send_file('../dist/index.html', cache_timeout=-1)
